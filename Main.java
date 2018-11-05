3204d841ec4bee8f3f913d9603b02bad8f5357cc
Michael-Bianconi <michael.a.bianconi@gmail.com>
2018-11-04 20:03:05 -0500

    Oh damn it works pretty well.

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import src.test.CmdArgsTest;
+import src.traceio.TraceIO;
 
 public class Main {
     
--- src/traceio/TraceIO.java
+++ src/traceio/TraceIO.java
@@ -26,12 +26,25 @@ public class TraceIO {
     private static final String BAD_OUTPUT_FILE =
         "Something went wrong with the output file!";
 
+    // How many pixels to look at when determining if to trace
+    private static final int TRACE_READ_SIZE = 1;
+
+    // How different the pixel has to be when determining to trace
+    private static final int TRACE_THRESHOLD = 0xFFFF8888;
+
+    // Color to trace with
+    private static final int TRACE_COLOR = 0xFFFFFF00;
+
+    // Color when not tracing
+    private static final int TRACE_NO_COLOR = 0xFF000000;
+
 
     private File inFile;
     private File outFile;
 
     private BufferedImage inImage;
-    private Graphics2D inGraphics;
+    private BufferedImage outImage;
+    private int[][] inRGB;
 
     /**
      * Reads the command line arguments into the input and output files.
@@ -54,21 +67,120 @@ public class TraceIO {
 
     /**
      * Creates the BufferedImage from the in instance variable file.
-     * Create the associated Graphics2D object as well.
      */
     private void readInputFile() {
 
+        // read in the image
         try {
 
             this.inImage = ImageIO.read(this.inFile);
-            this.inGraphics = inImage.createGraphics();
-
+    
         } catch (IOException e) {
 
             System.out.println(BAD_INPUT_FILE);
             System.out.println(e);
             System.exit(1);
         }
+
+        // get its RGB values
+        int width = this.inImage.getWidth();
+        int height = this.inImage.getHeight();
+        this.inRGB = new int[height][width];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+
+                //System.out.print(col + " " + row + " ");
+                this.inRGB[row][col] = inImage.getRGB(col, row);
+                //System.out.println(String.format("0x%08X", this.inRGB[row][col]));
+
+            }
+        }
+    }
+
+
+    /**
+     * Create the output file with the same dimensions as the input file.
+     * @req.me readInputFile() must have been called prior to this.
+     */
+    private void createOutputImage() {
+
+        this.outImage = new BufferedImage(inImage.getWidth(),
+                                          inImage.getHeight(),
+                                          BufferedImage.TYPE_INT_ARGB);
+    }
+
+
+    /**
+     * Get the difference of the given pixel from the surrounding pixels.
+     */
+    private int getDelta(int x, int y) {
+
+        int middle = this.inRGB[y][x];
+        int starty = y - TRACE_READ_SIZE;
+        int endy = y + TRACE_READ_SIZE;
+        int startx = x - TRACE_READ_SIZE;
+        int endx = x + TRACE_READ_SIZE;
+        int delta = 0;
+
+        if (starty < 0) { starty = 0; }
+        if (endy > inImage.getHeight()) { endy = inImage.getHeight(); }
+        if (startx < 0) { startx = 0; }
+        if (endx > inImage.getWidth()) { endx = inImage.getWidth(); }
+
+        for (int r = starty; r < endy; r++) {
+            for (int c = startx; c < endx; c++) {
+
+                delta += middle - inRGB[r][c];
+            }
+        }
+
+        return delta;
+    }
+
+
+    /**
+     * Trace!!!
+     */
+    private void trace() {
+
+        int numRows = this.inImage.getHeight() - TRACE_READ_SIZE;
+        int numCols = this.inImage.getWidth() - TRACE_READ_SIZE;
+
+        for (int row = TRACE_READ_SIZE; row < numRows; row++) {
+
+            for (int col = TRACE_READ_SIZE; col < numCols; col++) {
+
+                int delta = getDelta(col, row);
+
+                if (delta > TRACE_THRESHOLD) {
+
+                    this.outImage.setRGB(col, row, TRACE_COLOR);
+
+                } else {
+
+                    this.outImage.setRGB(col, row, TRACE_NO_COLOR);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Save the outImage to the outFile.
+     */
+    private void save() {
+
+        try {
+            ImageIO.write(this.outImage, "png", this.outFile);
+
+        } catch (IOException e) {
+
+            System.out.println(BAD_OUTPUT_FILE);
+            System.out.println(e);
+            System.exit(1);
+        }
+
     }
 
 
@@ -76,6 +188,9 @@ public class TraceIO {
 
         parseCommandLineArguments(args);
         readInputFile();
+        createOutputImage();
+        trace();
+        save();
 
 
 
